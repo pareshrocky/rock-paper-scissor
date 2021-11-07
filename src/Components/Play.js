7b5124372254fa9a5f2abb2e56c756af067f549b
paresh Sahoo <pareshrockyphry20002016@gmail.com>
2021-11-07 23:52:40 +0530

    done app

---
+++ src/Components/Play.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import Traingle from "../assets/images/bg-triangle.svg"
+
+const Play = ({setMyChoice}) => {
+    
+    const handleChoice = (e) => {
+        const val = e.target.dataset.id
+        setMyChoice(val)
+    }
+    
+    return (
+        <div className="play">
+            <img src={Traingle} alt="" className="traingle"/>
+            <div className="items">
+                <Link to="/game">
+                    <div data-id="paper" className="icon icon-paper" onClick={handleChoice}></div>
+                </Link>
+                <Link to="/game">
+                    <div data-id="scissors" className="icon icon-scissors" onClick={handleChoice}></div>
+                </Link>
+                <Link to="/game">
+                    <div data-id="rock" className="icon icon-rock" onClick={handleChoice}></div>
+                </Link>
+            </div>
+           
+        </div>
+    );
+};
+
+export default Play;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import App from "./App";
+import "./styles.scss";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    rootElement
+);
